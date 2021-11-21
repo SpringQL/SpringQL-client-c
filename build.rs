7f2c41d29070357fcc263ae7f06a7b5296dff1d2
Sho Nakatani <lay.sakura@gmail.com>
2021-11-22 08:28:20 +0900

    build: cbindgen::generate to use cbindgen.toml

--- build.rs
+++ build.rs
@@ -5,9 +5,8 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::Builder::new()
-        .with_crate(crate_dir)
-        .generate()
+    // uses cbindgen.toml
+    cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
-        .write_to_file("bindings.h");
+        .write_to_file("springql.h");
 }
