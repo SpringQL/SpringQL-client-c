614076ced3824351eddae005011b55c15547d108
Sho Nakatani <lay.sakura@gmail.com>
2021-11-22 09:39:40 +0900

    feat: spring_open

--- springql.h
+++ springql.h
@@ -4,6 +4,74 @@
 #include <stdlib.h>
 
 /**
- * x
+ * Errno (error number) to be returned erroneous functions.
+ *
+ * See springql_core::api::error::SpringError for details of each error reason.
  */
-void rust_function(void);
+typedef enum SpringErrno {
+  Ok = 0,
+  /**
+   * Panic
+   */
+  Unknown = -1,
+  ForeignIo = -2,
+  ForeignSourceTimeout = -3,
+  InputTimeout = -4,
+  SpringQlCoreIo = -5,
+  ThreadPoisoned = -6,
+  InvalidOption = -7,
+  InvalidFormat = -8,
+  Unavailable = -9,
+  Sql = -10,
+  /**
+   * Insufficient buffer size
+   */
+  CInsufficient = -126,
+  /**
+   * Invalid null pointer
+   */
+  CNull = -127,
+} SpringErrno;
+
+typedef SpringPipeline SpringPipeline;
+
+/**
+ * See: springql_core::api::spring_open
+ */
+enum SpringErrno spring_open(SpringPipeline *pipeline);
+
+/**
+ * Write the most recent error message into a caller-provided buffer as a UTF-8
+ * string, returning the number of bytes written.
+ *
+ * # Note
+ *
+ * This writes a **UTF-8** string into the buffer. Windows users may need to
+ * convert it to a UTF-16 "unicode" afterwards.
+ *
+ * If there are no recent errors then this returns `0` (because we wrote 0
+ * bytes). `-1` is returned if there are any errors, for example when passed a
+ * null pointer or a buffer of insufficient size.
+ *
+ * # Returns
+ *
+ * - `0`: if there are no recent errors.
+ * - `> 0`: the length of the recent error message.
+ * - `< 0`: SpringErrno
+ *
+ * # Safety
+ *
+ * This function is unsafe because it writes into a caller-provided buffer.
+ */
+int spring_last_errmsg(char *buffer, int length);
+
+/**
+ * Calculate the number of bytes in the last error's error message **not**
+ * including any trailing `null` characters.
+ *
+ * # Returns
+ *
+ * - `0`: if there are no recent errors.
+ * - `> 0`: the length of the recent error message.
+ */
+int spring_last_errlen(void);
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,23 @@
 //! C-API
 
+use springql_core::low_level_rs as springql_core;
+
 use spring_errno::SpringErrno;
 
 pub mod spring_errno;
 pub mod spring_last_errmsg;
 
+#[non_exhaustive]
+#[repr(transparent)]
+pub struct SpringPipeline(springql_core::SpringPipeline);
+
 /// See: springql_core::api::spring_open
 #[no_mangle]
-pub extern "C" fn spring_open() -> SpringErrno {
-    todo!()
+pub extern "C" fn spring_open(pipeline: *mut SpringPipeline) -> SpringErrno {
+    springql_core::spring_open()
+        .map(|p| unsafe {
+            *pipeline = SpringPipeline(p);
+            SpringErrno::Ok
+        })
+        .unwrap_or_else(|e| e.into())
 }
