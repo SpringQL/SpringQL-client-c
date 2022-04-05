e2107e3b2f707306c21595160de8e0b54cf184c8
Sho Nakatani <lay.sakura@gmail.com>
2022-04-05 18:47:06 +0900

    fix: url

--- springql.h
+++ springql.h
@@ -1,4 +1,4 @@
-// This file is part of https://github.com/SpringQL/SpringQL which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
+// This file is part of https://github.com/SpringQL/SpringQL-client-c which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
 
 #include <stdarg.h>
 #include <stdbool.h>
--- src/spring_errno.rs
+++ src/spring_errno.rs
@@ -1,4 +1,4 @@
-// This file is part of https://github.com/SpringQL/SpringQL which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
+// This file is part of https://github.com/SpringQL/SpringQL-client-c which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
 
 use springql_core::error::SpringError;
 
