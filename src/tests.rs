a3379380cf2fccd78ee3fa17de353c84a1e52a36
Sho Nakatani <lay.sakura@gmail.com>
2022-07-12 17:40:08 +0900

    test: add test_spring_open

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,5 @@
 // This file is part of https://github.com/SpringQL/SpringQL-client-c which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
 
 mod spring_config;
+mod spring_open;
 mod spring_source_row_builder;
