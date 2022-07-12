2aabeacd6e0fa3f22b5d6104863542e19ba1d001
Sho Nakatani <lay.sakura@gmail.com>
2022-07-12 16:02:56 +0900

    test: add test_spring_config_toml

--- src/tests/spring_config.rs
+++ src/tests/spring_config.rs
@@ -1,11 +1,23 @@
 // This file is part of https://github.com/SpringQL/SpringQL-client-c which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
 
+use std::ffi::CString;
+
 use crate::*;
 
 #[test]
-fn test_spring_config() {
+fn test_spring_config_default() {
     unsafe {
         let config = spring_config_default();
+        assert!(!config.is_null());
+        spring_config_close(config);
+    }
+}
+
+#[test]
+fn test_spring_config_toml() {
+    let toml = CString::new("").unwrap();
+    unsafe {
+        let config = spring_config_toml(toml.as_ptr());
         assert!(!config.is_null());
         spring_config_close(config);
     }
